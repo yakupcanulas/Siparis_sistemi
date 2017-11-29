114cfbf60e07e60162b22fc60b64510213354eb1
yakupcanulas <yakupcan_ulas@hotmail.com>
2017-11-29 23:03:31 +0300

    Sipariş Sistemi

---
+++ 09082017/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace _09082017
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lstsepet = new System.Windows.Forms.ListBox();
+            this.lsttümsiparis = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lstürünler = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "ANA YEMEKLER",
+            "SALATALAR",
+            "MEŞRUBAT",
+            "TATLILAR"});
+            this.comboBox1.Location = new System.Drawing.Point(15, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(143, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lstsepet
+            // 
+            this.lstsepet.FormattingEnabled = true;
+            this.lstsepet.Location = new System.Drawing.Point(288, 90);
+            this.lstsepet.Name = "lstsepet";
+            this.lstsepet.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstsepet.Size = new System.Drawing.Size(120, 121);
+            this.lstsepet.TabIndex = 1;
+            // 
+            // lsttümsiparis
+            // 
+            this.lsttümsiparis.FormattingEnabled = true;
+            this.lsttümsiparis.Location = new System.Drawing.Point(430, 90);
+            this.lsttümsiparis.Name = "lsttümsiparis";
+            this.lsttümsiparis.Size = new System.Drawing.Size(253, 121);
+            this.lsttümsiparis.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(193, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(193, 135);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = ">>>";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(193, 173);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "<<<";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(430, 252);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(161, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "SİPARİŞİ TAMAMLA";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(333, 252);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "ÇIKIŞ";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(288, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(395, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(285, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "MÜŞTERİ ADI GİRİNİZ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 16);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "KATEGORİ SEÇİNİZ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(285, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "SEPET";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lstürünler
+            // 
+            this.lstürünler.FormattingEnabled = true;
+            this.lstürünler.Location = new System.Drawing.Point(15, 56);
+            this.lstürünler.Name = "lstürünler";
+            this.lstürünler.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstürünler.Size = new System.Drawing.Size(143, 160);
+            this.lstürünler.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(288, 218);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "0";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(12, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(129, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "İNDİRİM MİKTARI";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton1.Location = new System.Drawing.Point(15, 252);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 20);
+            this.radioButton1.TabIndex = 16;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "%5";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton2.Location = new System.Drawing.Point(15, 275);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(55, 20);
+            this.radioButton2.TabIndex = 17;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "%10";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton3.Location = new System.Drawing.Point(15, 298);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(55, 20);
+            this.radioButton3.TabIndex = 18;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "%15";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(712, 333);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lstürünler);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lsttümsiparis);
+            this.Controls.Add(this.lstsepet);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox lstsepet;
+        private System.Windows.Forms.ListBox lsttümsiparis;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lstürünler;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ 09082017/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _09082017
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstürünler.Items.Clear();
+            
+            if (comboBox1.SelectedIndex==0)
+            {
+                lstürünler.Items.Add("Abdo Usulü Et Döner");
+                lstürünler.Items.Add("Oruk");
+                lstürünler.Items.Add("Sini Kebabaı");
+                lstürünler.Items.Add("Kaytaz Böreği");
+            }
+
+            else if (comboBox1.SelectedIndex==1)
+            {
+                lstürünler.Items.Add("Çökelek Salatası");
+                lstürünler.Items.Add("Zeytin Öfelemesi");
+                lstürünler.Items.Add("Çoban Salata");
+            }
+
+            else if (comboBox1.SelectedIndex==2)
+            {
+                lstürünler.Items.Add("Şerbet");
+                lstürünler.Items.Add("Şalgam");
+                lstürünler.Items.Add("Limonata");
+            }
+
+            else if (comboBox1.SelectedIndex==3)
+            {
+                lstürünler.Items.Add("Sade Künefe");
+                lstürünler.Items.Add("Kaymaklı Künefe");
+                lstürünler.Items.Add("Şam Tatlısı");
+                lstürünler.Items.Add("Baklava");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int adet = Convert.ToInt32(label4.Text);
+            lstsepet.Items.Add(lstürünler.SelectedItem);
+            adet++;
+            label4.Text = adet.ToString();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int adet = Convert.ToInt32(label4.Text);    
+                    
+            lstsepet.Items.Remove(lstsepet.SelectedItem);
+            adet--;
+            label4.Text = adet.ToString();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            int adet;
+            adet = Convert.ToInt32(label4.Text);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int adet;
+
+            adet = Convert.ToInt32(label4.Text);
+
+            for (int i = 0; i < lstürünler.SelectedItems.Count; i++)
+            {
+                lstsepet.Items.Add(lstürünler.SelectedItems[i]);
+                adet++;
+                
+            }
+            label4.Text = adet.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int adet;
+            int cikarilan = lstsepet.SelectedItems.Count;
+            adet = Convert.ToInt32(label4.Text);
+            for (int i = 0; i < cikarilan; i++)
+            {
+                lstsepet.Items.Remove(lstsepet.SelectedItems[0]);
+
+
+                
+            }
+            if (adet > 0)
+            {
+                adet = adet - cikarilan;
+            }
+
+
+            label4.Text = adet.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                lsttümsiparis.Items.Add(textBox1.Text + " " +"ürün sayısı " + label4.Text  + "," + "indirim yüzdesi"+ " " +radioButton1.Text );
+            }
+
+            else if (radioButton2.Checked)
+            {
+                lsttümsiparis.Items.Add(textBox1.Text + " " + "ürün sayısı "  + label4.Text + "," + "indirim yüzdesi"+ " " + radioButton2.Text);
+            }
+            else if (radioButton3.Checked)
+            {
+                lsttümsiparis.Items.Add(textBox1.Text + " " + "ürün sayısı "  + label4.Text + "," + "indirim yüzdesi"+ " " + radioButton3.Text);
+            }
+
+            else
+            {
+                lsttümsiparis.Items.Add(textBox1.Text + " " + "ürün sayısı " + label4.Text + ","+ "indirim uygulanmamıştır");
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
